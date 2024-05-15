f9ee6387c30ebfe0e9495974730857e05c8c9bfe
Rashidov01 <avazbekrashidov6@gmail.com>
2024-05-16 00:56:24 +0400

    anything

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
 import React from "react";
-import{BrowserRouter,Switch,Route}from 'react-router-dom' 
+// import{BrowserRouter,Switch,Route}from 'react-router-dom'
 import "./App.css";
-import Header from "./components/Header.js";
-import Home from "./pages/Home.js";
-import Profile from "./pages/Profile.js";
+// import Header from "./components/Header.js";
+// import Home from "./pages/Home.js";
+// import Profile from "./pages/Profile.js";
 import Payment from "./pages/Payment.js";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <div className="mainContainer">
-          <Header />
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/profile" component={Profile} />
-            <Route path="/payment" component={Payment} />
-          </Switch>
-        </div>
-      </div>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <div className="App">
+    //     <div className="mainContainer">
+    //       <Header />
+    //       <Switch>
+    //         {/* <Route path="/" exact component={Home} />
+    //         <Route path="/profile" component={Profile} /> */}
+    //         <Route path="/payment" component={Payment} />
+    //       </Switch>
+    //     </div>
+    //   </div>
+    // </BrowserRouter>
+    <Payment />
   );
 }
 
--- frontend/src/pages/Payment.js
+++ frontend/src/pages/Payment.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from "react";
+import "../styles/Payment.css";
+import PaymentIcon from "../images/payment1.svg";
 
 function Payment() {
   return (
-    <div>
-      <h1>Payment Page</h1>
-      {/* Add your content here */}
+    <div className="payment">
+      <img src={PaymentIcon} alt="payment icon" />
+      <div>
+        <h2>Pending Payments</h2>
+        <ul>
+          <li>
+            <span>1</span>
+            <p>Red signal fine</p>
+            <p className="day day-early day-late">2 days left</p>
+            <p className="price">220 AED</p>
+            <button>pay</button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
