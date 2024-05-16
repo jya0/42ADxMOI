b3499741eb42c591ab2f4410e999ec5e60897363
Rashidov01 <avazbekrashidov6@gmail.com>
2024-05-16 07:15:42 +0400

    init audio

---
+++ frontend/src/components/Audio.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Audio() {
+  return (
+    <>
+      <canvas id="canvas"></canvas>
+      <button onClick="start()">Start</button>
+    </>
+  );
+}
